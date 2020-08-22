091eed9bae3b7c7463b5309429804ce5e7964c1c
furu38 <full.ryu13@gmail.com>
2020-08-23 00:42:36 +0900

    Add Delete: rename root directory
    rename directory 01_root to 00_root

---
+++ source/env/env.tf
@@ -0,0 +1,13 @@
+variable "envs" {}
+variable "region" {}
+
+// リソース・マネージャに配置することで自動補完される
+variable "tenancy_ocid" {}
+
+provider "oci" {
+	region = "ap-tokyo-1"
+}
+
+terraform {
+	required_version = "~> 0.12"
+}
